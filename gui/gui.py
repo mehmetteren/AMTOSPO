0773698f452f1fbd371b630fc187f91029fe9f2b
mehmetteren <eren.balasar@ug.bilkent.edu.tr>
2022-10-09 18:01:43 +0300

    edit

--- gui/gui.py
+++ gui/gui.py
@@ -4,42 +4,47 @@ from tkinter import messagebox as ms
 
 class GUI:
 
-    window = tk.Tk()
-    canvas = tk.Canvas(window, height=400, width=700)
-    canvas.pack()
+    def __init__(self):
+        window = tk.Tk()
+        canvas = tk.Canvas(window, height=400, width=700)
+        canvas.pack()
 
-    frame_left = tk.Frame(window, bg='grey')
-    frame_left.place(relx=0, rely=0, relheight=1, relwidth=0.12)
+        frame_left = tk.Frame(window, bg='grey')
+        frame_left.place(relx=0, rely=0, relheight=1, relwidth=0.12)
 
-    frame_rightUp = tk.Frame(window, bg='light grey')
-    frame_rightUp.place(relx=0.13, rely=0, relheight=0.5, relwidth=0.87)
+        frame_rightUp = tk.Frame(window, bg='light grey')
+        frame_rightUp.place(relx=0.13, rely=0, relheight=0.5, relwidth=0.87)
 
-    frame_rightDown = tk.Frame(window, bg='light grey')
-    frame_rightDown.place(relx=0.13, rely=0.52, relheight=0.48, relwidth=0.87)
+        frame_rightDown = tk.Frame(window, bg='light grey')
+        frame_rightDown.place(relx=0.13, rely=0.52, relheight=0.48, relwidth=0.87)
 
-    tk.Label(frame_rightUp, bg='light grey', text='Welcome to amtospo!', font="Verdana 25 bold").pack(anchor='s',
-                                                                                                      padx=25,
-                                                                                                      pady=25)
+        tk.Label(frame_rightUp, bg='light grey', text='Welcome to amtospo!', font="Verdana 25 bold").pack(anchor='s',
+                                                                                                          padx=25,
+                                                                                                          pady=25)
 
-    tk.Label(frame_rightUp, bg='light grey', text='Playlist from: ', font="Verdana 17 bold").pack(anchor='sw', padx=10,
-                                                                                                  pady=10)
+        tk.Label(frame_rightUp, bg='light grey', text='Playlist from: ', font="Verdana 17 bold").pack(anchor='sw',
+                                                                                                      padx=10,
+                                                                                                      pady=10)
 
-    transType_default = tk.StringVar(frame_rightUp)
-    transType_default.set("\t")
+        transType_default = tk.StringVar(frame_rightUp)
+        transType_default.set("\t")
 
-    transType_menu = tk.OptionMenu(frame_rightUp, transType_default, 'Youtube Music', 'Apple Music')
-    transType_menu.pack(padx=10, pady=10, anchor='sw')
+        transType_menu = tk.OptionMenu(frame_rightUp, transType_default, 'Youtube Music', 'Apple Music')
+        transType_menu.pack(padx=10, pady=10, anchor='sw')
 
-    tk.Label(frame_rightDown, bg='light grey', text='File name or Url: ', font="Verdana 17 bold").pack(anchor='nw',
-                                                                                                       padx=10,
-                                                                                                       pady=10)
+        tk.Label(frame_rightDown, bg='light grey', text='File name or Url: ', font="Verdana 17 bold").pack(anchor='nw',
+                                                                                                           padx=10,
+                                                                                                           pady=10)
 
-    text_space = tk.Text(frame_rightDown, bg='light blue', height=0.5, width=50)
-    text_space.tag_configure('style', foreground='#bfbfbf', font=('Verdana', 7, 'bold'))
-    text_space.pack(anchor='nw', padx=10, pady=10)
+        text_space = tk.Text(frame_rightDown, bg='light blue', height=0.5, width=50)
+        text_space.tag_configure('style', foreground='#bfbfbf', font=('Verdana', 7, 'bold'))
+        text_space.pack(anchor='nw', padx=10, pady=10)
 
-    text = 'Paste link or enter XML file name...'
-    text_space.insert('end', text, 'style')
+        text = 'Paste link or enter XML file name...'
+        text_space.insert('end', text, 'style')
+
+        send_button = tk.Button(frame_rightDown, text='Send', command=self.send)
+        send_button.pack(anchor='sw', padx=10, pady=10)
 
     #
     def send(self):
@@ -49,13 +54,6 @@ class GUI:
             message = 'Please enter informations correctly!'
 
         ms.showwarning('Error', message)
-
-    send_button = tk.Button(frame_rightDown, text='Send', command=send)
-    send_button.pack(anchor='sw', padx=10, pady=10)
-
-    def __init__(self):
-        return
-
 
 
 
--- musicMove.py
+++ musicMove.py
@@ -1,12 +1,12 @@
-import applemusic_source_parser as axp
-import applemusic_xml_parser as apxml
+import parsers.applemusic_source_parser as axp
+import parsers.applemusic_xml_parser as apxml
 import spotify_accessor as sa
 import ui as ui
 import gui.gui as gui
 
 print('Welcome to Music Move!\n')
 
-guiObject()
+guiObject = gui.GUI()
 
 if gui.send() == 'Youtube Music':
     final_song_name = axp.get_song_name()
